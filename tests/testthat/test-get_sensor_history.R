ac8f1d5b11e0057c0dc3eb79727ed8443797afbc
Cole Brokamp <cole.brokamp@gmail.com>
2026-05-08 16:15:38 -0400

    don't mock; use real calls for tests

--- tests/testthat/test-get_sensor_history.R
+++ tests/testthat/test-get_sensor_history.R
@@ -2,7 +2,10 @@ test_that("get_sensor_history works", {
   skip_on_os(c("windows", "linux", "solaris"))
   skip_on_cran()
   testthat::skip_if_offline()
-  testthat::skip_if(Sys.getenv("PURPLE_AIR_API_KEY") == "", "no PurpleAir API key present")
+  testthat::skip_if(
+    Sys.getenv("PURPLE_AIR_API_KEY") == "",
+    "no PurpleAir API key present"
+  )
   get_sensor_history(
     sensor_index = 175413,
     fields = c("pm1.0_cf_1", "pm1.0_atm", "pm2.5_cf_1", "pm2.5_atm"),
@@ -34,35 +37,19 @@ test_that("get_sensor_history rejects invalid sensor indices before requesting",
 })
 
 test_that("get_sensor_history maps 1week average to the API value", {
-  request_average <- NULL
-
-  testthat::local_mocked_bindings(
-    purple_air_request = function(..., average) {
-      request_average <<- average
-      list()
-    },
-    .env = environment(get_sensor_history)
+  skip_on_os(c("windows", "linux", "solaris"))
+  skip_on_cran()
+  testthat::skip_if_offline()
+  testthat::skip_if(
+    Sys.getenv("PURPLE_AIR_API_KEY") == "",
+    "no PurpleAir API key present"
   )
-  testthat::local_mocked_bindings(
-    req_perform = identity,
-    resp_body_json = function(resp) {
-      force(resp)
-      list(
-        fields = c("time_stamp", "pm2.5_atm"),
-        data = list(list(1719878400, 5.5))
-      )
-    },
-    .package = "httr2"
-  )
-
-  out <- get_sensor_history(
+  get_sensor_history(
     sensor_index = 175413,
     fields = "pm2.5_atm",
     start_timestamp = as.POSIXct("2024-07-02", tz = "UTC"),
     end_timestamp = as.POSIXct("2024-07-09", tz = "UTC"),
     average = "1week"
-  )
-
-  expect_identical(request_average, 10080L)
-  expect_s3_class(out, "tbl_df")
+  ) |>
+    expect_s3_class("tbl_df")
 })
